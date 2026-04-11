cfb1db8251c4104e648aa5cd507fd84262a5733c
ngocbao-net <nbao281223@gmail.com>
2026-04-11 09:46:32 +0700

    Main

--- paper_simulations/single_simulation_with_map/point_in_segment.m
+++
@@ -1,4 +0,0 @@
-function res = point_in_segment(pt, a, b, eps)
-% POINT_IN_SEGMENT Returns true if point is in segment, false otherwise, with tolerance eps
-    res = abs(norm(pt-a) + norm(pt-b) - norm(a-b)) < eps;
-    
